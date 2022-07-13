e701550c9abf69fbe22ccc1c3cdc09895f26c159
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-07-13 15:52:17 +0200

    array with custom types

--- src/index.js
+++ src/index.js
@@ -1,22 +1,72 @@
 import { GraphQLServer} from 'graphql-yoga';
 
+// Demo user data
+const users = [{
+  id: '1',
+  name: 'Leonoor',
+  email: 'leonoor@example.com',
+  age: 27
+}, {
+  id: '2',
+  name: 'Sara',
+  email: 'sara@example.com'
+}, {
+  id: '3',
+  name: 'Mike',
+  email: 'mike@example.com'
+
+}]
+
 // Type definitions, application schema
 const typeDefs = `
   type Query {
-    add(numbers: [Float!]!): Float!
+    users(query: String): [User!]!
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `
 
 // Resolvers
 const resolvers = {
   Query: {
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
       }
 
-      return args.numbers.reduce((a,b) => a + b)
-    }
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
+    },
+    me() {
+      return {
+        id: '123ABC',
+        name: 'Leonoor Test',
+        email: 'leonoor@testable.com'
+      }
+    },
+    post() {
+      return {
+        id: '456DEF',
+        title: 'This is my title',
+        body: 'This is the contents of my post.',
+        published: false
+      }
+    } 
   }
 }
 
